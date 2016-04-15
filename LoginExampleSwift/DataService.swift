2b449f82583b10a33e1d0699444b56a9af3507d9
smoenkemeyer <sonja.moenkemeyer@zalando.de>
2016-04-15 18:16:46 +0200

    add dataservice

--- LoginExampleSwift/DataService.swift
+++ LoginExampleSwift/DataService.swift
@@ -11,5 +11,26 @@ import Firebase
 
 class DataService {
     static let dataService = DataService()
+
+    private var _BASE_REF = Firebase(url: "\(BASE_URL)")
+    private var _USER_REF = Firebase(url: "\(BASE_URL)/users")
+
+    var BASE_REF: Firebase {
+        return _BASE_REF
+    }
+
+    var USER_REF: Firebase {
+        return _USER_REF
+    }
+
+    var CURRENT_USER_REF : {
+        let userID = NSUserDefaults.standardUserDefaults().valueForKey("uid") as! String
+
+        let currentUser = Firebase(url: "\(BASE_REF)").childByAppendingPath("users").childByAppendingPatch(userID)
+
+        return currentUser!
+    }
+
+
     
 }
